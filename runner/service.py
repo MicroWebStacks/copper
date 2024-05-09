c3ecc71c792c951a4e2077cb0d26fd547ed8383a
wass <wassim.filali@gmail.com>
2024-05-09 17:30:52 +0200

    moved compose

--- runner/service.py
+++ runner/service.py
@@ -15,22 +15,22 @@ async def run():
     print("async run()")
     workflow = load_yaml("/app/workflow.yaml")
     print("workflow loaded - waiting for event")
-    async with Client(BROKER) as client:
-        await client.publish("runner/status","up")
-        for action in workflow:
-            finish_topic = action["action"] + "/finish"
-            await client.publish(f"runner/start", payload=json.dumps(action))
-            await client.subscribe(finish_topic)
-            await client.publish(action["action"], payload=json.dumps(action))
-            async for message in client.messages:
-                print(f"received message on {message.topic}")
-                print(message.payload)
-                break
-            await client.publish(f"runner/finish", payload=json.dumps(action))
+    client = Client(BROKER, PORT, will=("runner/status", "disconnected", 2, True))
+    await client.publish("runner/status","connected")
+    for action in workflow:
+        finish_topic = action["action"] + "/finish"
+        await client.publish(f"runner/start", payload=json.dumps(action))
+        await client.subscribe(finish_topic)
+        await client.publish(action["action"], payload=json.dumps(action))
+        async for message in client.messages:
+            print(f"received message on {message.topic}")
+            print(message.payload)
+            break
+        await client.publish(f"runner/finish", payload=json.dumps(action))
+    await client.disconnect()
     return
 
 BROKER = 'host.docker.internal'
 PORT = 1883
-CACHE_PATH = "/cache"
 
 asyncio.run(run())
